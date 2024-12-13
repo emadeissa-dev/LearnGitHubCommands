ad3878507fe11f4f850d4833e8dcc761c03613de
emadeissa-dev <emad.eissa999333@gmail.com>
2024-12-13 22:13:56 +0200

    Add project files.

---
+++ LearnGitHubCommands/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+///Learn GitHub
+// git add [FileName]
+//git status [See Changes]
+//git commit -m "Name Of Changes"
+//git rm [FileName] // To Delete
+// git push => To Push
+// git config --global user.name => get User
+// git config --global user.email => get Email
