dc7655198106c3933e037d4f8ed27c353b03ecf0
changeName <emad.eissa999333@gmail.com>
2024-12-13 22:56:09 +0200

    rfrfrf

--- LearnGitHubCommands/Program.cs
+++ LearnGitHubCommands/Program.cs
@@ -8,6 +8,10 @@ Console.WriteLine("Hello, World!");
 //git commit -m "Name Of Changes"
 //git rm [FileName] // To Delete
 // git push => To Push
+// git pull
 // git config --global user.name => get User
 // git config --global user.email => get Email
 // git config --global user.name "changeName"
+// git config -l --show-origin
+//git remote => Origin
+//git branch => master
